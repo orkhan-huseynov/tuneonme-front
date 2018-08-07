3b5798fb2a10f34945a3fad40c93f23a26ef720e
orkhan.huseynov@live.com <orkhan.huseynov@live.com>
2018-08-07 22:44:09 +0400

    signup form, check if email exists

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import config from '../config';
+import axios from 'axios';
 import { Button, Form, FormGroup, FormFeedback, Input, Label, Col } from 'reactstrap';
 
 class SignUp extends React.Component {
     constructor(props) {
         super(props);
 
+        this.handleEmailBlur = this.handleEmailBlur.bind(this);
         this.handleSignUpClick = this.handleSignUpClick.bind(this);
         this.handleAlreadyRegisteredClick = this.handleAlreadyRegisteredClick.bind(this);
 
@@ -12,17 +15,34 @@ class SignUp extends React.Component {
             nameIsValid: undefined,
             lastNameIsValid: undefined,
             emailIsValid: undefined,
+            emailExists: undefined,
             passwordIsValid: undefined,
             confirmPasswordIsValid: undefined,
         }
     }
 
+    handleEmailBlur(e) {
+        const inputEmail = document.getElementById('inputEmail');
+        if (inputEmail.value !== '') {
+            axios.post(config.emailExistsUrl, {
+                email: inputEmail.value,
+            })
+                .then(res => {
+                    const data = res.data;
+                    const emailExists = data.responseContent;
+                    this.setState({emailExists: emailExists, emailIsValid: !emailExists});
+                });
+        } else {
+            this.setState({emailExists: false});
+        }
+    }
+
     handleSignUpClick(e) {
-        let inputName = document.getElementById('inputName');
-        let inputLastName = document.getElementById('inputLastName');
-        let inputEmail = document.getElementById('inputEmail');
-        let inputPassword = document.getElementById('inputPassword');
-        let inputConfirmPassword = document.getElementById('inputConfirmPassword');
+        const inputName = document.getElementById('inputName');
+        const inputLastName = document.getElementById('inputLastName');
+        const inputEmail = document.getElementById('inputEmail');
+        const inputPassword = document.getElementById('inputPassword');
+        const inputConfirmPassword = document.getElementById('inputConfirmPassword');
 
         let validationSuccessful = true;
 
@@ -40,11 +60,13 @@ class SignUp extends React.Component {
             this.setState({lastNameIsValid: true});
         }
 
-        if (inputEmail.value === '' || !(/\S+@\S+\.\S+/.test(inputEmail.value))) {
-            this.setState({emailIsValid: false});
-            validationSuccessful = false;
-        } else {
-            this.setState({emailIsValid: true});
+        if (!this.state.emailExists) {
+            if (inputEmail.value === '' || !(/\S+@\S+\.\S+/.test(inputEmail.value))) {
+                this.setState({emailIsValid: false});
+                validationSuccessful = false;
+            } else {
+                this.setState({emailIsValid: true});
+            }
         }
 
         if (inputPassword.value === '') {
@@ -81,27 +103,49 @@ class SignUp extends React.Component {
                 <Form className="LogInSignUpForm" noValidate>
                     <FormGroup>
                         <Label for="inputName" className="sr-only">Name</Label>
-                        <Input type="text" id="inputName" placeholder="Name" name="name" valid={this.state.nameIsValid === true} invalid={this.state.nameIsValid === false} required autoFocus />
+                        <Input type="text" id="inputName" placeholder="Name" name="name"
+                               valid={this.state.nameIsValid === true}
+                               invalid={this.state.nameIsValid === false}
+                               required autoFocus
+                        />
                         <FormFeedback>Please enter your name</FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="inputLastName" className="sr-only">Last name</Label>
-                        <Input type="text" id="inputLastName" placeholder="Last name" name="lastName" valid={this.state.lastNameIsValid === true} invalid={this.state.lastNameIsValid === false} required />
+                        <Input type="text" id="inputLastName" placeholder="Last name" name="lastName"
+                               valid={this.state.lastNameIsValid === true}
+                               invalid={this.state.lastNameIsValid === false}
+                               required
+                        />
                         <FormFeedback>Please enter your last name</FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="inputEmail" className="sr-only">Email address</Label>
-                        <Input type="email" id="inputEmail" placeholder="Email address" name="email" valid={this.state.emailIsValid === true} invalid={this.state.emailIsValid === false} required />
-                        <FormFeedback>Please enter a valid email</FormFeedback>
+                        <Input type="email" id="inputEmail" placeholder="Email address" name="email"
+                               valid={this.state.emailIsValid === true}
+                               invalid={this.state.emailIsValid === false}
+                               required
+                               onBlur={this.handleEmailBlur}
+                        />
+                        <FormFeedback>{ this.state.emailExists ? 'Email already exists' : 'Please enter a valid email' }</FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="inputPassword" className="sr-only">Password</Label>
-                        <Input type="password" id="inputPassword" placeholder="Password" name="password" valid={this.state.passwordIsValid === true} invalid={this.state.passwordIsValid === false}  required />
+                        <Input type="password" id="inputPassword" placeholder="Password" name="password"
+                               valid={this.state.passwordIsValid === true}
+                               invalid={this.state.passwordIsValid === false}
+                               required
+                        />
                         <FormFeedback>Password is required</FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="inputConfirmPassword" className="sr-only">Confirm password</Label>
-                        <Input type="password" id="inputConfirmPassword" placeholder="Confirm password" name="passwordConfirmation" valid={this.state.confirmPasswordIsValid === true} invalid={this.state.passwordIsValid === false} required />
+                        <Input type="password" id="inputConfirmPassword" placeholder="Confirm password"
+                               name="passwordConfirmation"
+                               valid={this.state.confirmPasswordIsValid === true}
+                               invalid={this.state.passwordIsValid === false}
+                               required
+                        />
                         <FormFeedback>Password confirmation is required</FormFeedback>
                     </FormGroup>
 
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const baseUrl = 'http://tuneon.me';
+
+let config = {
+    emailExistsUrl: `${baseUrl}/api/email_exists`,
+};
+
+export default config;
